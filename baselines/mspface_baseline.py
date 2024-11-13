7cacd896d2a349eb9d158c5738b46d1dd74fda89
Thomas Bahmandeji <bahmandejithomas@gmail.com>
2024-11-13 10:55:02 -0700

    Beginning MSP_Face baseline(s)

---
+++ baselines/mspface_baseline.py
@@ -0,0 +1,9 @@
+mspface_segments_dir = "/media/data5/thomas/MSP_Face_segments"
+mspface_dir = "/media/data5/thomas/MSP_Face"
+mspface_doc_dir = f'{mspface_dir}/Doc'
+
+def classify_mspface():
+
+
+def main():
+    classify_mspface()
